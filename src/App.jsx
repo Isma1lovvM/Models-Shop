9261eda23423ae49db399f2c2fda3ae35a92e54b
Isma1lovvM <isma1lovv.m.h.n@gmail.com>
2026-02-21 14:56:25 +0500

    projects is edited

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,26 @@
+import { useEffect } from "react";
 import "./App.css";
+import Header from "./components/header/Header";
+import ScrollToTop from "./components/ScrollToTop";
+
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function App() {
-  return <></>;
+  useEffect(() => {
+    AOS.init({
+      duration: 1000,
+      once: true,
+    });
+  }, []);
+
+  return (
+    <>
+      <ScrollToTop />
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,96 @@
+import "header.css";
+
+import { useState } from "react";
+import { CgProfile } from "react-icons/cg";
+import { FaBars } from "react-icons/fa6";
+import { IoCloseOutline } from "react-icons/io5";
+import { RiSearchLine } from "react-icons/ri";
+import { NavLink } from "react-router-dom";
+
+import cartBtn from "../../assets/cartBtn.svg";
+
+function Header() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  return (
+    <>
+      <div className="container">
+        <header>
+          <div className="navBox">
+            <nav className="navbar">
+              <div className="container">
+                <div className="containerTexts">
+                  <div className="logoElBox">
+                    <button
+                      onClick={() => setIsMenuOpen(true)}
+                      className="bar"
+                      aria-label="Open menu"
+                    >
+                      <FaBars />
+                    </button>
+
+                    <NavLink to="/">
+                      <h1 className="logoText">
+                        3legant<span className="gray">.</span>
+                      </h1>
+                    </NavLink>
+                  </div>
+
+                  <ul className={`navList ${isMenuOpen ? "menuActive" : ""}`}>
+                    <div className="barListBox">
+                      <h3>3legant</h3>
+                      <button
+                        className="closeMenuBtn"
+                        onClick={() => setIsMenuOpen(false)}
+                        aria-label="Close menu"
+                      >
+                        <IoCloseOutline />
+                      </button>
+                    </div>
+
+                    {[
+                      { path: "/", name: "Home" },
+                      { path: "/shop", name: "Shop" },
+                    ].map((item) => (
+                      <li key={item.path}>
+                        <NavLink
+                          to={item.path}
+                          className={({ isActive }) =>
+                            isActive ? "active" : "navLink"
+                          }
+                          onClick={() => setIsMenuOpen(false)}
+                        >
+                          {item.name}
+                        </NavLink>
+                      </li>
+                    ))}
+                  </ul>
+
+                  <ul className="navIconsList">
+                    <RiSearchLine className="icon" aria-label="Search" />
+                    <CgProfile className="icon" aria-label="Profile" />
+                    <img
+                      className="icon"
+                      src={cartBtn}
+                      alt="cart button"
+                      aria-label="Cart button"
+                    />
+                  </ul>
+
+                  {isMenuOpen && (
+                    <div
+                      className="overlay"
+                      onClick={() => setIsMenuOpen(false)}
+                    ></div>
+                  )}
+                </div>
+              </div>
+            </nav>
+          </div>
+        </header>
+      </div>
+    </>
+  );
+}
+
+export default Header;
